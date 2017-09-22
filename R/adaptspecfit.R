05174a9f541839d9393bbbe77adc6fd5668636e4
Michael Bertolacci <m.bertolacci@gmail.com>
2017-09-22 09:03:02 +0800

    Reduce the verbosity of the diagnostic output

--- R/adaptspecfit.R
+++ R/adaptspecfit.R
@@ -59,10 +59,12 @@ diagnostics.adaptspecfit <- function(fit, iterations_threshold = 0) {
     n_iterations <- sum(fit$n_segments == n_segments)
     if (n_iterations < iterations_threshold) next
     cat(sprintf('--- For n_segments = %d, number of iterations = %d\n', n_segments, n_iterations))
-    for (segment in 1 : n_segments) {
-      cat('Segment', segment, '\n')
-      print(coda::rejectionRate(coda::mcmc(fit$beta[fit$n_segments == n_segments, segment, ])))
-    }
+
+    rejection_rates <- sapply(1 : n_segments, function(segment) {
+      coda::rejectionRate(coda::mcmc(fit$beta[fit$n_segments == n_segments, segment, 1]))
+    })
+    names(rejection_rates) <- 1 : n_segments
+    print(rejection_rates)
   }
 }
 
